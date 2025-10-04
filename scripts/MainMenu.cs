87eba926711c20b41fdf7a81336a8693709c77ee
trashstarrrrr <164434094+trashstarrrrr@users.noreply.github.com>
2025-10-04 18:09:08 +0300

    mainmenuanimation

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -1,6 +1,11 @@
 using Godot;
 using System;
 
+[Export] PackedScene CharacterSelect
 public partial class MainMenu : Control
 {
+    private void _on_new_button_pressed()
+    {
+        GetTree().ChangeScene("res://")
+    }
 }
